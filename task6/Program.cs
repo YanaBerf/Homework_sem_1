b62d3214640cfe266c7345233a1f80190c0dd500
[YanaB] <[tudabae@gmail.com]>
2023-06-22 15:42:02 +0300

    четное ли число

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0){
+    Console.WriteLine($"{n} - четное");
+}else{
+    Console.WriteLine($"{n} - не является четным");
+}
