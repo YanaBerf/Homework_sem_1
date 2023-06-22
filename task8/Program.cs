1f4f2fdb6299da7a5a13daaba3ce6216d0ebec37
[YanaB] <[tudabae@gmail.com]>
2023-06-22 17:17:14 +0300

    четное число от 1 до N

---
+++ task8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+// int i = 2;
+//while (i <= n ){
+   
+   // Console.Write($"{i}");
+    //i = i + 2;//}
+for (i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+    Console.Write($"{i+","}");
+    }
+ }
+
+
+
+
+
+
+
+
