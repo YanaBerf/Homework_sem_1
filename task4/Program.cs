5fd4e328894cfa43c4e9ff6a6f364fb968db4260
[YanaB] <[tudabae@gmail.com]>
2023-06-22 15:28:01 +0300

    нахождение макс из 3-х чисел

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("число первое: ");
+int a = int.Parse(Console.ReadLine()); 
+Console.Write("число второе: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("число третье: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine($"max = {a}");
+}
+if (b > a && b > c){
+    Console.WriteLine($"max = {b}");
+}
+if (c > a && c > b){
+    Console.WriteLine($"max = {c}");
+}
