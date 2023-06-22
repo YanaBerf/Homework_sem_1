921098993b94f89c29ca19188d6969550261afb8
[YanaB] <[tudabae@gmail.com]>
2023-06-22 12:38:46 +0300

    add task1.1

---
+++ task1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b){
+    Console.WriteLine($"max = {a}");
+} 
+else{
+    Console.WriteLine($"max = {b}");
+}
